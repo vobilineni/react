e71d8d7dbc243064d29982c03b18272343b68935
Srinivas Vobilineni <SV317@ntrs.com>
2018-07-04 07:09:30 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+      var myStyle ={
+          fontSize: 100,
+          color: '#F00'
+      }
+      var i = 1;
+    return (
+      <div>
+          <h1 style ={myStyle}>{i === 1 ? 'true':'false'}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
